0a525bcbf6fad5dcfa63dec62dd1d2527a999eca
dhnki <88179675+dhnki@users.noreply.github.com>
2022-01-05 18:24:04 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>Hello {{ value }}</div>
+	<v-app>
+		<v-app-bar app>
+			<AppHeader></AppHeader>
+		</v-app-bar>
+
+		<v-main>
+			<router-view></router-view>
+		</v-main>
+		<Snackbar></Snackbar>
+	</v-app>
 </template>
 
 <script>
+import AppHeader from '@/components/common/AppHeader'
+import Snackbar from '@/components/common/Snackbar'
+
 export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  }
-};
+	name: 'App',
+	components: {
+		AppHeader,
+		Snackbar,
+	},
+	data: () => ({
+		//
+	}),
+}
 </script>
